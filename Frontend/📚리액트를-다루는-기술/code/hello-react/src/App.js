b4186d948454740934eedfbb0c8bc2cc4e563c85
eunbi lee <led156@naver.com>
2024-06-20 02:40:56 +0900

    3.2. 첫 컴포넌트 생성

--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = '리액트';
-  return (
-    <div className="react">
-      {name}
-    </div>
-  );
-}
+const App = () => {
+  return <MyComponent />;
+};
 
 export default App;
