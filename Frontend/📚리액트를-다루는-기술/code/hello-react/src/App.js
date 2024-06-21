f88c3edf176832373acd41767f4c5088e2b69ee0
eunbi lee <led156@naver.com>
2024-06-21 21:13:38 +0900

    3.3. props

--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return <MyComponent name="React" favoriteNumber={3}>리액트</MyComponent>;
 };
 
 export default App;
--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/MyComponent.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/MyComponent.js
@@ -1,18 +1,29 @@
-const MyComponent = ({ name, children }) => {
+import PropsTypes from 'prop-types'
+
+const MyComponent = ({ name = '기본 이름', favoriteNumber, children }) => {
 
     /* const { name, children } = props; */
 
     return (
         <div>
             나의 새롭고 멋진 {name} <br />
-            chileren 값은 {children} 입니다.
+            chileren 값은 {children} 입니다. <br />
+            제가 좋아하는 숫자는 {favoriteNumber} 입니다.
         </div>
     );
 };
 
-/* props 기본값 설정 */
-MyComponent.defaultProps = {
-    name: '기본 이름'
+/* propsType 설정 */
+MyComponent.PropsTypes = {
+    name: PropsTypes.string,
+    favoriteNumber: PropsTypes.number.isRequired
 };
+
+/* props 기본값 설정 */
+// MyComponent.defaultProps = {
+//     name: '기본 이름'
+// };
+
+
 
 export default MyComponent;
