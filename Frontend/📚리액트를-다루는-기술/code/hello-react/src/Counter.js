4e678f603e48df90e6b44b5c2ab0e88e29234bdf
eunbi lee <led156@naver.com>
2024-06-21 22:09:45 +0900

    3.4

--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/Counter.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/Counter.js
@@ -14,16 +14,29 @@ class Counter extends Component {
                 <button
                 // onClick 을 통하여 버튼이 클릭됐을 때 호출 할 함수를 지정합니다.
                 onClick={() => {
+                    /* <2> 2씩 증가 */
+                    // this.setState(prevState => {
+                    //   return {
+                    //     number: prevState.number + 1
+                    //   };
+                    // });
+                    // // 위 코드와 아래 코드는 완전히 똑같은 기능을 하는 코드입니다.
+                    // // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
+                    // this.setState(prevState => ({
+                    //   number: prevState.number + 1
+                    // }));
+                    
+                    /* <1> 1씩 증가 */
                     // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
                     this.setState(
-                    {
-                        number: number + 1
-                    },
-                    () => {
-                        console.log('방금 setState 가 호출되었습니다.');
-                        console.log(this.state);
-                    }
-                );
+                        {
+                            number: number + 1
+                        },
+                        () => {
+                            console.log('방금 setState 가 호출되었습니다.');
+                            console.log(this.state);
+                        }
+                    );
             }}
             >
                 +1
