e6bac6a9d2ee5c9ae1a363a15abf02b2d828ea62
eunbi lee <led156@naver.com>
2024-06-21 20:52:12 +0900

    3.3.4

--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/MyComponent.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/MyComponent.js
@@ -1,5 +1,14 @@
-const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = (props) => {
+    return (
+    <div>나의 새롭고 멋진 {props.name} <br />
+    chileren 값은 {props.children} 입니다.
+    </div>)
+    ;
+};
+
+/* props 기본값 설정 */
+MyComponent.defaultProps = {
+    name: '기본 이름'
 };
 
 export default MyComponent;
