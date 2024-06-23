8ee2b3018f257f330c69467e5fe53fad307cb4d4
eunbi lee <led156@naver.com>
2024-06-23 18:24:50 +0900

    5장 ref

--- Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
+++ Frontend/📚리액트를-다루는-기술/code/hello-react/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { Component } from 'react';
 import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return <ScrollBox />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
